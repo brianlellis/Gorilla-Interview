4ed531db95f40c65d980d0107f43c37db1ce7448
Brian Ellis <Brian@BriansResume.com>
2017-07-16 21:32:49 -0400

    Fixed mock width sizing

--- test/specs/search-auto-complete.js
+++ test/specs/search-auto-complete.js
@@ -4,6 +4,7 @@ browser.url('/');
 describe('fixture', () => {
   it('Search auto complete is functional', () => {
   	browser.setValue('#searchInput','pr');
+  	browser.pause(100);
   	assert.ok( browser.isVisible('.autocomplete-suggestions') );
   });
 });
--- test/specs/size-check.js
+++ test/specs/size-check.js
@@ -3,9 +3,23 @@ browser.url('/');
 browser.windowHandleSize({width:1900,height:800});
 
 describe('fixture', () => {
+  it('Page container is correct width', () => {
+    const mainCont = browser.getElementSize('#content','width'); 
+    assert.equal( mainCont, 1170);
+  });
+});
+
+describe('fixture', () => {
   it('Sidebar is correct width', () => {
     const sidebarWidth = browser.getElementSize('//div[@class="sidebar"]','width'); 
     assert.equal( (sidebarWidth >= 230 && sidebarWidth <= 235) ? 233 : 0, 233);
+  });
+});
+
+describe('fixture', () => {
+  it('Product Grid is correct width', () => {
+    const prodGrid = browser.getElementSize('#productGrid','width'); 
+    assert.equal( (prodGrid >= 868 && prodGrid <= 873) ? 870 : 0, 870);
   });
 });
 
